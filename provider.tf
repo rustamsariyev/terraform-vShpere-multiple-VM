852b1d54ab927fef3447115f64c52feba0a180c2
Rustam Sariyev <r.sariyev@outlook.com>
2019-07-25 12:20:22 +0400

    Terraform Provider file fixed

--- provider.tf
+++ provider.tf
@@ -2,6 +2,7 @@ provider "vsphere" {
   user           = "${var.username}"
   password       = "${var.password}"
   vsphere_server = "${var.vcenter}"
-
+ 
   # If you have a self-signed cert
   allow_unverified_ssl = true
+}
